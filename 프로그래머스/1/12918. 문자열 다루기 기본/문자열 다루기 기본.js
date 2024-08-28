b92eedb15991a576247488f67f9f1ab7df8932db
tjdcjf1996 <r_louis@naver.com>
2024-08-28 11:56:52 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return s.length===4 || s.length=== 6 ? s === Number.parseInt(s).toString() ? true:false : false;
+    return s.length===4 || s.length=== 6 ? (s === [...s].map(a=> 48<=a.charCodeAt() && a.charCodeAt()<=57 ? a : "").join("") ? true:false) : false;
 }

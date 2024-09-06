e38020241612c85a7d2712f5c67f423fef0d477b
tjdcjf1996 <r_louis@naver.com>
2024-09-06 09:57:49 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -33,3 +33,35 @@ function solution(survey, choices) {
     
     return answer;
 }
+
+
+// 더 효율적인 코드
+// function solution(survey, choices) {
+//     let answer = "";
+//     const personality = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+    
+//     // 각 질문에 따른 선택을 반영
+//     for (let i = 0; i < survey.length; i++) {
+//         const [disagree, agree] = survey[i];  // 성격 유형 쌍
+//         const score = choices[i] - 4;  // 4를 기준으로 점수 계산 (중립은 0)
+        
+//         if (score < 0) {
+//             personality[disagree] += Math.abs(score);  // 비동의 선택
+//         } else if (score > 0) {
+//             personality[agree] += score;  // 동의 선택
+//         }
+//     }
+
+//     // 성격 유형 쌍에 따라 더 점수가 높은 쪽을 선택
+//     const pairs = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']];
+    
+//     pairs.forEach(([first, second]) => {
+//         if (personality[first] >= personality[second]) {
+//             answer += first;
+//         } else {
+//             answer += second;
+//         }
+//     });
+    
+//     return answer;
+// }

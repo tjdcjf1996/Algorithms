a46e0fba9237696aa07bb373aa9fe768c7b16d5e
tjdcjf1996 <r_louis@naver.com>
2024-09-06 09:21:14 +0900

    [level 1] Title: 햄버거 만들기, Time: 289.49 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,15 @@
+function solution(ingredient) {
+    const stack = [];
+    let answer = 0;
+    
+    for(let i=0; i<ingredient.length; i++){
+        stack.push(ingredient[i]);
+        if(stack.length>=4 && stack.slice(-4).join('') === '1231'){
+            stack.splice(-4);
+            answer++;
+        }
+    }
+    
+    
+    return answer;
+}

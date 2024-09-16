5364aba643a133f486897ab30ee837cddc320a25
tjdcjf1996 <r_louis@naver.com>
2024-09-16 21:27:20 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,18 +1,17 @@
 function solution(brown, yellow) {
     const sum = brown + yellow;
-    const x = [];
-    const y = [];
-   
-    // x 구하기
-    for(let i=1; i<=Math.sqrt(sum); i++ ){
-        if( sum % i === 0 ){
-          x.push(sum/i);
-          y.push(i);
-        }    
-    }
-    for(let i=0; i<x.length; i++){
-        const yellowCount = (x[i]-2) * (y[i]-2);
-        if(yellowCount === yellow) return [x[i],y[i]];
-    }
     
+    // 가로와 세로 길이 
+    for (let i = 1; i <= Math.sqrt(sum); i++) {
+        if (sum % i === 0) {
+            const x = sum / i;  // 가로
+            const y = i;       // 세로
+            
+            // 타일의 갯수가 맞는지 확인
+            const yellowCount = (x - 2) * (y - 2);
+            if (yellowCount === yellow) {
+                return [x, y];
+            }
+        }
+    }
 }

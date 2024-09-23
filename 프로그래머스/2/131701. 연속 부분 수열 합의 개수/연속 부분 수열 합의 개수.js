3fb7c180b65efbbb897a3b76a12469555f224a3a
tjdcjf1996 <r_louis@naver.com>
2024-09-23 09:34:17 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2797.87 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,21 +1,16 @@
 function solution(elements) {
     const sumSet = new Set();
     
-    for(let i=1; i<=elements.length; i++){ 
-        let start = 0;
+    for (let i = 1; i <= elements.length; i++) {
         let sum = 0;
-        while(start<elements.length){
-            for(let j=0; j<i; j++){
-                sum += elements[(start+j)%(elements.length)];
+        for (let start = 0; start < elements.length; start++) {
+            for (let j = 0; j < i; j++) { 
+                sum += elements[(start + j) % elements.length]; 
             }
-            start ++;
             sumSet.add(sum);
-            sum = 0;
+            sum = 0; 
         }
     }
     
-    const answer = [...sumSet].length;
-
-    return answer;
-    
+    return sumSet.size;
 }

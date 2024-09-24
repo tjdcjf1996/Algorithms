4b2d0f5001a544f4664178c45dc76a3478bd5e07
tjdcjf1996 <r_louis@naver.com>
2024-09-24 09:48:07 +0900

    [level 2] Title: H-Index, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -2,7 +2,6 @@ function solution(citations) {
 
     const sortedCitations = citations.sort((a,b)=>b-a);
     const length = sortedCitations.length;
-    let answer = 0;
     
     for(let i=0; i<length; i++){
         if(sortedCitations[i]<i+1) return i;

7c1c86bfd804c8390da5129b37afb3a5a7308938
tjdcjf1996 <r_louis@naver.com>
2024-10-01 09:38:57 +0900

    [level 2] Title: 프로세스, Time: 1.40 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,13 +1,13 @@
 function solution(priorities, location) {
     const processArr = priorities.reduce((acc,priority,idx) => {
-        acc.push([priority,65+idx]);
+        acc.push([priority,idx]);
         return acc;
     },[]);
     let number = 0;
     while(processArr.length>0){
         const maxPriority = Math.max(...processArr.map(item => item[0]));
         const temp = processArr.shift();
-        if(temp[0] === maxPriority && temp[1] === 65+location){
+        if(temp[0] === maxPriority && temp[1] === location){
             number ++;
             return number;
         }else if(temp[0] === maxPriority){
